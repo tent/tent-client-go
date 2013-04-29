a36864b6a760c81464af1fabe20ca30c55a6fce2
Jonathan Rudenberg <jonathan@titanous.com>
2013-04-28 20:07:08 -0400

    Split meta into a separate file

--- discovery.go
+++ discovery.go
@@ -2,84 +2,12 @@ package tent
 
 import (
 	"bytes"
-	"encoding/json"
 	"io"
 	"mime"
-	"net/url"
-	"strings"
 
 	"code.google.com/p/go.net/html"
 	"github.com/tent/http-link-go"
 )
-
-const RelMetaPost = "https://tent.io/rels/meta-post"
-
-type MetaPostServer struct {
-	Version    string `json:"version"`
-	Preference int    `json:"preference"`
-
-	URLs MetaPostServerURLs `json:"urls"`
-}
-
-type MetaPostServerURLs struct {
-	OAuthAuth      string `json:"oauth_auth"`
-	OAuthToken     string `json:"oauth_token"`
-	PostsFeed      string `json:"posts_feed"`
-	Post           string `json:"post"`
-	NewPost        string `json:"new_post"`
-	PostAttachment string `json:"post_attachment"`
-	Attachment     string `json:"attachment"`
-	Batch          string `json:"batch"`
-	ServerInfo     string `json:"server_info"`
-}
-
-func (urls *MetaPostServerURLs) PostURL(entity, post, version string) (string, error) {
-	u := strings.Replace(urls.Post, "{entity}", url.QueryEscape(entity), 1)
-	u = strings.Replace(u, "{post}", post, 1)
-	if version != "" {
-		if strings.Contains(u, "?") {
-			uri, err := url.Parse(u)
-			if err != nil {
-				return "", err
-			}
-			q := uri.Query()
-			q.Add("version", version)
-			uri.RawQuery = q.Encode()
-			u = uri.String()
-		} else {
-			u += "?version=" + version
-		}
-	}
-	return u, nil
-}
-
-func (urls *MetaPostServerURLs) PostAttachmentURL(entity, post, name, version string) string {
-	u := strings.Replace(urls.PostAttachment, "{entity}", url.QueryEscape(entity), 1)
-	u = strings.Replace(u, "{post}", post, 1)
-	u = strings.Replace(u, "{name}", url.QueryEscape(name), 1)
-	return strings.Replace(u, "{version}", version, 1)
-}
-
-func (urls *MetaPostServerURLs) AttachmentURL(entity, digest string) string {
-	u := strings.Replace(urls.Attachment, "{entity}", url.QueryEscape(entity), 1)
-	return strings.Replace(u, "{digest}", digest, 1)
-}
-
-type MetaPost struct {
-	Entity  string           `json:"entity"`
-	Servers []MetaPostServer `json:"servers"`
-	Post    *Post            `json:"-"`
-}
-
-func GetMetaPost(url string) (*MetaPost, error) {
-	post, err := GetPost(url)
-	if err != nil {
-		return nil, err
-	}
-	metaPost := &MetaPost{Post: post}
-	err = json.Unmarshal(post.Content, metaPost)
-	return metaPost, err
-}
 
 func Discover(entity string) (*MetaPost, error) {
 	req, err := newRequest("HEAD", entity, nil, nil)
@@ -148,21 +76,6 @@ func Discover(entity string) (*MetaPost, error) {
 	}
 
 	return nil, nil
-}
-
-func getMetaPost(links []string, reqURL *url.URL) (*MetaPost, error) {
-	for i, l := range links {
-		u, err := url.Parse(l)
-		if err != nil {
-			return nil, err
-		}
-		m, err := GetMetaPost(reqURL.ResolveReference(u).String())
-		if err != nil && i < len(links)-1 {
-			continue
-		}
-		return m, err
-	}
-	panic("not reached")
 }
 
 func parseHTMLMetaLinks(data io.Reader) (links []string, err error) {

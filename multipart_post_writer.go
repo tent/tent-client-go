0a13f63f54bdb96493a838a0cd60b6510e0edc5c
Jonathan Rudenberg <jonathan@titanous.com>
2013-06-10 22:41:39 -0400

    Use int64 for attachment Len()

--- multipart_post_writer.go
+++ multipart_post_writer.go
@@ -11,7 +11,7 @@ import (
 
 type ReadLenSeeker interface {
 	io.ReadSeeker
-	Len() int
+	Len() int64
 }
 
 type MultipartPostWriter struct {
@@ -25,7 +25,7 @@ func NewMultipartPostWriter(w io.Writer) *MultipartPostWriter {
 
 func (w *MultipartPostWriter) WritePost(post *Post) error {
 	data, _ := json.Marshal(post)
-	part, err := w.m.CreatePart(mimeFileHeader("post", "post.json", post.contentType(), len(data)))
+	part, err := w.m.CreatePart(mimeFileHeader("post", "post.json", post.contentType(), int64(len(data))))
 	if err != nil {
 		return err
 	}
@@ -42,11 +42,11 @@ func (w *MultipartPostWriter) WriteAttachment(att *PostAttachment) error {
 func (w *MultipartPostWriter) Close() error        { return w.m.Close() }
 func (w *MultipartPostWriter) ContentType() string { return w.m.FormDataContentType() }
 
-func mimeFileHeader(name, filename, contentType string, size int) textproto.MIMEHeader {
+func mimeFileHeader(name, filename, contentType string, size int64) textproto.MIMEHeader {
 	h := make(textproto.MIMEHeader)
 	h.Set("Content-Disposition", `form-data; name="`+escapeQuotes(name)+`"; filename="`+escapeQuotes(filename)+`"`)
 	h.Set("Content-Type", contentType)
-	h.Set("Content-Length", strconv.Itoa(size))
+	h.Set("Content-Length", strconv.FormatInt(size, 10))
 	return h
 }
 

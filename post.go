fc7c1ae358f364e98340592011f2ec91a9871696
Jonathan Rudenberg <jonathan@titanous.com>
2013-06-23 16:21:08 -0400

    Add post version calculation logic

--- post.go
+++ post.go
@@ -2,6 +2,8 @@ package tent
 
 import (
 	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"io"
@@ -11,24 +13,26 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/cupcake/sfilter"
+	"github.com/tent/canonical-json-go"
 	"github.com/tent/hawk-go"
 	"github.com/tent/http-link-go"
 )
 
 type PostRef struct {
-	Entity         string `json:"entity,omitempty"`
+	Entity         string `json:"entity,omitempty" sfilter:"version"`
 	OriginalEntity string `json:"original_entity,omitempty"`
-	Post           string `json:"post,omitempty"`
-	Version        string `json:"version,omitempty"`
-	Type           string `json:"type,omitempty"`
+	Post           string `json:"post,omitempty" sfilter:"version"`
+	Version        string `json:"version,omitempty" sfilter:"version"`
+	Type           string `json:"type,omitempty" sfilter:"version"`
 }
 
 type PostMention struct {
-	Entity         string `json:"entity,omitempty"`
+	Entity         string `json:"entity,omitempty" sfilter:"version"`
 	OriginalEntity string `json:"original_entity,omitempty"`
-	Post           string `json:"post,omitempty"`
-	Version        string `json:"version,omitempty"`
-	Type           string `json:"type,omitempty"`
+	Post           string `json:"post,omitempty" sfilter:"version"`
+	Version        string `json:"version,omitempty" sfilter:"version"`
+	Type           string `json:"type,omitempty" sfilter:"version"`
 	PublicFlag     *bool  `json:"public,omitempty"` // nil or true is public == true; false is public == false
 }
 
@@ -37,11 +41,11 @@ func (mention *PostMention) Public() bool {
 }
 
 type PostAttachment struct {
-	Name        string `json:"name"`
-	Category    string `json:"category"`
-	ContentType string `json:"content_type"`
-	Size        int64  `json:"size,omitempty"`
-	Digest      string `json:"digest,omitempty"`
+	Name        string `json:"name" sfilter:"version"`
+	Category    string `json:"category" sfilter:"version"`
+	ContentType string `json:"content_type" sfilter:"version"`
+	Size        int64  `json:"size,omitempty" sfilter:"version"`
+	Digest      string `json:"digest,omitempty" sfilter:"version"`
 
 	// Include Data to upload a new attachment with the post
 	Data ReadLenSeeker `json:"-"`
@@ -86,23 +90,23 @@ func (perm *PostPermissions) Public() bool {
 }
 
 type PostApp struct {
-	Name string `json:"name,omitempty"`
-	URL  string `json:"url,omitempty"`
+	Name string `json:"name,omitempty sfilter:"version""`
+	URL  string `json:"url,omitempty" sfilter:"version"`
 	ID   string `json:"id,omitempty"`
 }
 
 type PostVersionParent struct {
-	Entity         string `json:"entity,omitempty"`
+	Entity         string `json:"entity,omitempty" sfilter:"version"`
 	OriginalEntity string `json:"original_entity,omitempty"`
-	Post           string `json:"post,omitempty"`
-	Version        string `json:"version"`
+	Post           string `json:"post,omitempty" sfilter:"version"`
+	Version        string `json:"version" sfilter:"version"`
 }
 
 type PostVersion struct {
-	ID          string              `json:"id,omitempty"`
-	Parents     []PostVersionParent `json:"parents,omitempty"`
-	Message     string              `json:"message,omitempty"`
-	PublishedAt *UnixTime           `json:"published_at,omitempty"`
+	ID          string              `json:"id,omitempty" sfilter:"version"`
+	Parents     []PostVersionParent `json:"parents,omitempty" sfilter:"version"`
+	Message     string              `json:"message,omitempty" sfilter:"version"`
+	PublishedAt *UnixTime           `json:"published_at,omitempty" sfilter:"version"`
 	ReceivedAt  *UnixTime           `json:"received_at,omitempty"`
 
 	// Used in post version and children lists
@@ -112,30 +116,30 @@ type PostVersion struct {
 }
 
 type PostLicense struct {
-	URL string `json:"url"`
+	URL string `json:"url" sfilter:"version"`
 }
 
 type Post struct {
-	ID string `json:"id,omitempty"`
+	ID string `json:"id,omitempty" sfilter:"version"`
 
-	Entity         string `json:"entity,omitempty"`
+	Entity         string `json:"entity,omitempty" sfilter:"version"`
 	OriginalEntity string `json:"original_entity,omitempty"`
 
-	Type    string          `json:"type"`
-	Content json.RawMessage `json:"content,omitempty"`
+	Type    string          `json:"type" sfilter:"version"`
+	Content json.RawMessage `json:"content,omitempty" sfilter:"version"`
 
-	Version *PostVersion `json:"version,omitempty"`
+	Version *PostVersion `json:"version,omitempty" sfilter:"version"`
 
-	Refs        []PostRef         `json:"refs,omitempty"`
-	Mentions    []PostMention     `json:"mentions,omitempty"`
-	Licenses    []PostLicense     `json:"licenses,omitempty"`
-	Attachments []*PostAttachment `json:"attachments,omitempty"`
+	Refs        []PostRef         `json:"refs,omitempty" sfilter:"version"`
+	Mentions    []PostMention     `json:"mentions,omitempty" sfilter:"version"`
+	Licenses    []PostLicense     `json:"licenses,omitempty" sfilter:"version"`
+	Attachments []*PostAttachment `json:"attachments,omitempty" sfilter:"version"`
 	Permissions *PostPermissions  `json:"permissions,omitempty"`
 
-	App *PostApp `json:"app,omitempty"`
+	App *PostApp `json:"app,omitempty" sfilter:"version"`
 
 	ReceivedAt  *UnixTime `json:"received_at,omitempty"`
-	PublishedAt *UnixTime `json:"published_at,omitempty"`
+	PublishedAt *UnixTime `json:"published_at,omitempty" sfilter:"version"`
 
 	Links []link.Link `json:"-"`
 
@@ -252,6 +256,85 @@ func (post *Post) hasNewAttachments() bool {
 		}
 	}
 	return false
+}
+
+func (post *Post) CalculateVersion() (string, error) {
+	data, _ := sfilter.Map(post, "version")
+
+	if post.OriginalEntity != "" {
+		data["entity"] = post.OriginalEntity
+	}
+
+	if data["mentions"] != nil {
+		postMentions := data["mentions"].([]map[string]interface{})
+		mentions := make([]map[string]interface{}, 0, len(postMentions))
+		for i, mention := range post.Mentions {
+			if !mention.Public() {
+				continue
+			}
+			if mention.Entity == post.Entity {
+				delete(postMentions[i], "entity")
+			}
+			if mention.Post == post.ID {
+				delete(postMentions[i], "post")
+			}
+			if mention.OriginalEntity != "" && post.OriginalEntity != mention.OriginalEntity {
+				postMentions[i]["entity"] = mention.OriginalEntity
+			}
+			if len(postMentions[i]) == 0 {
+				continue
+			}
+			mentions = append(mentions, postMentions[i])
+		}
+		data["mentions"] = mentions
+		if len(mentions) == 0 {
+			delete(data, "mentions")
+		}
+	}
+
+	if data["refs"] != nil {
+		refs := data["refs"].([]map[string]interface{})
+		for i, ref := range post.Refs {
+			if ref.Entity == post.Entity {
+				delete(refs[i], "entity")
+			}
+			if ref.Post == post.ID {
+				delete(refs[i], "post")
+			}
+			if ref.OriginalEntity != "" && post.OriginalEntity != ref.OriginalEntity {
+				refs[i]["entity"] = ref.OriginalEntity
+			}
+		}
+	}
+
+	if len(post.Version.Parents) > 0 {
+		version := data["version"].(map[string]interface{})
+		parents := version["parents"].([]map[string]interface{})
+		for i, parent := range post.Version.Parents {
+			if parent.Entity == post.Entity {
+				delete(parents[i], "entity")
+			}
+			if parent.Post == post.ID {
+				delete(parents[i], "post")
+			}
+			if parent.OriginalEntity != "" && post.OriginalEntity != parent.OriginalEntity {
+				parents[i]["entity"] = parent.OriginalEntity
+			}
+		}
+	}
+
+	if len(post.Content) < 3 {
+		delete(data, "content")
+	}
+
+	canonicalJSON, err := cjson.Marshal(data)
+	if err != nil {
+		return "", err
+	}
+
+	h := sha512.New()
+	h.Write(canonicalJSON)
+	return hex.EncodeToString(h.Sum(nil)[:32]), nil
 }
 
 func (post *Post) contentType() string {
